e6f1dd18cb66dc0581ccb891c690b6c40d14258b
mahletm2020 <mahimahlet2002@gmail.com>
2026-01-30 10:53:24 +0300

    initail se up

---
+++ back/itas/src/main/java/com/itas/itas/ItasApplication.java
@@ -0,0 +1,13 @@
+package com.itas.itas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItasApplication.class, args);
+	}
+
+}
---
+++ back/itas/src/test/java/com/itas/itas/ItasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itas.itas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
